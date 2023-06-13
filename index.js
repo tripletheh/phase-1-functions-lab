3a18492198a459d1c64383f75ea88f9776eb971d
Harrison_Hilson <harryhilson8@gmail.com>
2023-06-13 11:56:31 -0400

    function stuff

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = (street)=>{
+    return Math.abs(street - 42);
+}
+const distanceFromHqInFeet = (street)=>{
+    return distanceFromHqInBlocks(street)*264;
+}
+const distanceTravelledInFeet = (start, destination) =>{
+    return Math.abs(destination - start)*264;
+}
+const calculatesFarePrice = (start, destination) => {
+    const distance =distanceTravelledInFeet(start, destination);
+    var charge;
+    if (distance<=400){
+        charge=0;
+    }
+    else if( distance >400 && distance <2000){
+        charge = (distance-400)*0.02;
+    }
+    else if (distance <=2500){
+        charge = 25;
+    }
+    else {
+        charge ="cannot travel that far";
+    }
+    return charge;
+}
